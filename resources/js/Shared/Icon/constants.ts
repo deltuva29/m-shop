7da301a4f72c1177a01b82b100a92615a6bd1378
Mindaugas Deltuva <deltuva.mindaugas@gmail.com>
2024-05-25 03:59:25 +0300

    wip: Add icon constants

---
+++ resources/js/Shared/Icon/constants.ts
@@ -0,0 +1,3 @@
+export const ICON_CART = 'cart';
+export const ICON_PLUS = 'plus';
+export const ICON_MINUS = 'minus';
--- resources/js/Shared/Icon/ui.vue
+++ resources/js/Shared/Icon/ui.vue
@@ -1,23 +1,24 @@
 <script lang="ts" setup>
 import { type IconType } from './types';
 import { MinusIcon, PlusIcon, ShoppingBagIcon } from "@heroicons/vue/24/outline";
+import { ICON_CART, ICON_MINUS, ICON_PLUS } from "@/Shared/Icon/constants";
 
 interface Props {
   type: IconType;
 }
 
 const props = defineProps<Props>();
-const {type} = props;
+const { type } = props;
 </script>
 
 <template>
   <ShoppingBagIcon
-    v-if="type === 'cart'"
+    v-if="type === ICON_CART"
     class="h-5 w-5"/>
   <MinusIcon
-    v-if="type === 'minus'"
+    v-if="type === ICON_MINUS"
     class="h-5 w-5"/>
   <PlusIcon
-    v-if="type === 'plus'"
+    v-if="type === ICON_PLUS"
     class="h-5 w-5"/>
 </template>
